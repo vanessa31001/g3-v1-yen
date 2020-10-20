8192a5a3b754204c2eda2647cb02e658caf35c11
vanessa31001 <vanessa31001@gmail.com>
2020-10-20 15:54:55 +0800

    救回下拉式選單

--- js/main.js
+++ js/main.js
@@ -8,30 +8,30 @@ $(function(){
 });
 
 // 首頁滑鼠移動效果
-// $(function() {
-//     var $el = $('.home');
-//     var mousePos = {};
-//     var boxCenter = {
-//         x: $el.outerWidth() / 2,
-//         y: $el.outerHeight() / 2
-//     };
-//     var bgPos = 50;
+$(function() {
+    var $el = $('.home');
+    var mousePos = {};
+    var boxCenter = {
+        x: $el.outerWidth() / 2,
+        y: $el.outerHeight() / 2
+    };
+    var bgPos = 50;
         
-//     $el.mousemove(function(e) {
-//         var x = e.offsetX === undefined ? e.layerX : e.offsetX;
-//         var y = e.offsetY === undefined ? e.layerY : e.offsetY;
+    $el.mousemove(function(e) {
+        var x = e.offsetX === undefined ? e.layerX : e.offsetX;
+        var y = e.offsetY === undefined ? e.layerY : e.offsetY;
         
-//         x -= boxCenter.x;
-//         y -= boxCenter.y;
+        x -= boxCenter.x;
+        y -= boxCenter.y;
         
-//         var moveX = bgPos + (x / 14);
-//         var moveY = bgPos + (y / 14);
-//         $el
-//         .css('background-size', '110%')
-//         .css('background-position', moveX +'% ' + moveY +'%');
-//     })
-//     .mouseleave(function() {
-//         $el
-//         .css('background-size', '100%');
-//     });
-// });
+        var moveX = bgPos + (x / 14);
+        var moveY = bgPos + (y / 14);
+        $el
+        .css('background-size', '110%')
+        .css('background-position', moveX +'% ' + moveY +'%');
+    })
+    .mouseleave(function() {
+        $el
+        .css('background-size', '100%');
+    });
+});
